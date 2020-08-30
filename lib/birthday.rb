bb68c48448669fea59ed6c22745004373ae57b0f
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-30 03:05:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,7 +17,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else
+    elsif
       puts "You are to old for this."
    end
  end
